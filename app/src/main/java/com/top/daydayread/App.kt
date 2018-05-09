e05f805cf3e6666c20143207297c604cf3783f7e
ProZoom <18642118+ProZoom@users.noreply.github.com>
2018-05-09 18:34:18 +0800

    Initial commit

---
+++ app/src/main/java/com/top/daydayread/App.kt
@@ -0,0 +1,18 @@
+package com.top.daydayread
+
+import android.app.Application
+import org.jetbrains.anko.AnkoLogger
+
+/**
+ * 作者：李阳
+ * 时间：2018/5/9
+ * 描述：
+ */
+class App: Application(),AnkoLogger {
+
+    override fun onCreate() {
+        super.onCreate()
+
+
+    }
+}
---
+++ app/src/main/java/com/top/daydayread/view/activity/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.top.daydayread.view.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import com.top.daydayread.R
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+
+}
