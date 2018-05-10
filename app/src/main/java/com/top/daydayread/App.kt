d4228120d3d8f891d2c2926671fd446c8c4851f9
ProZoom <18642118+ProZoom@users.noreply.github.com>
2018-05-10 19:04:09 +0800

    510
    510

--- app/src/main/java/com/top/daydayread/App.kt
+++ app/src/main/java/com/top/daydayread/App.kt
@@ -1,7 +1,11 @@
 package com.top.daydayread
 
 import android.app.Application
+import android.provider.Settings
+import com.tencent.bugly.Bugly
+import com.tencent.bugly.crashreport.CrashReport
 import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.ctx
 
 /**
  * 作者：李阳
@@ -10,9 +14,15 @@ import org.jetbrains.anko.AnkoLogger
  */
 class App: Application(),AnkoLogger {
 
+    var BuglyID="e9a3a42baf"
+
     override fun onCreate() {
         super.onCreate()
+        initBugly()
 
+    }
 
+    private fun initBugly(){
+        Bugly.init(this,BuglyID,false) // 第三个参数为SDK调试模式开关，
     }
 }
